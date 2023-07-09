1d10c1dfcc8e31799b690e9683e843a5f23f0266
lamendolara <lamendolara@gmail.com>
2023-07-09 12:06:33 +0200

    configuracion del proyecto

---
+++ usuarios/usuarios/settings/local.py
@@ -0,0 +1,33 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+
+]
+# STATICFILES_DIRS = [BASE_DIR/'static']
+# STATICFILES_DIRS = [BASE_DIR.child('static')] Este era el que decia el del curso, pero veo que no funciona.
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
